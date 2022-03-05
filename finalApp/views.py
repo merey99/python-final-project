39f768342382a613fc5a5b6c8b1eb05cb6d21b61
Gani Gainelov <gainelov666@gmail.com>
2022-03-05 14:18:33 +0600

    initial commit

--- finalApp/views.py
+++ finalApp/views.py
@@ -7,7 +7,6 @@ from django.shortcuts import render
 from keras.models import load_model
 from keras.preprocessing import image
 from tensorflow import Graph
-
 from finalApp.models import Image
 
 img_height, img_width = 224, 224
@@ -23,6 +22,11 @@ with model_graph.as_default():
         model = load_model('./models/MobileNetModelImagenet.h5')
 
 
+def showHistory(request):
+    images = Image.objects.all()
+    context = {'images': images}
+    return render(request, 'history.html', context)
+
 def index(req):
     context = {'image': "", 'label': ""}
     return render(req, 'index.html', context)
@@ -31,33 +35,20 @@ def index(req):
 def classifyImg(req):
     fObj = req.FILES['imagePath']
     fStorage = FileSystemStorage()
-
     imgPath = fStorage.save(fObj.name, fObj)
     imgPath = fStorage.url(imgPath)
-
     img = '.' + imgPath
     img = image.load_img(img, target_size=(img_height, img_width))
-
     imgArr = image.img_to_array(img)
     imgArr = imgArr / 255
     imgArr = imgArr.reshape(1, img_height, img_width, 3)
-
     with model_graph.as_default():
         with session.as_default():
             prediction = model.predict(imgArr)
-
     pLabel = labelList[str(np.argmax(prediction[0]))]
-
     dbImage = Image()
     dbImage.path = imgPath
     dbImage.predicted_label = pLabel[1]
     dbImage.save()
-
     context = {'image': imgPath, 'label': pLabel[1]}
     return render(req, 'index.html', context)
-
-
-def showHistory(request):
-    images = Image.objects.all()
-    context = {'images': images}
-    return render(request, 'history.html', context)
